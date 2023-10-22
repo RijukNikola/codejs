13a7ae53c49cfb5afc28704a0bdc00d390b075c6
RijukNikola <kola1997@icloud.com>
2023-10-22 15:43:22 +0500

    solution to problem 7

--- style.js
+++ style.js
@@ -185,9 +185,9 @@ function catchSignChange(arr) {
 
  sortByLength(["", "Moderately", "Brains", "Pizza"])
 
- 
 
-// 6
+
+// 7
 
 //  In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
@@ -195,3 +195,16 @@ function catchSignChange(arr) {
 // filter_list([1,2,'a','b']) == [1,2]
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+ //################ SOLUTION
+
+ function filter_list(l) {
+	let arr = []
+	for (let i = 0; i < l.length; i++) {
+		if (typeof l[i]  === "number") {
+			arr.push(l[i])
+		}
+	} 
+	return arr
+ }
+ filter_list([1,2,'aasf','1','123',123])
