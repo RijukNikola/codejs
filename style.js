c3aaba446f9fee98cb358f4e12baa7b470e103d2
RijukNikola <kola1997@icloud.com>
2023-10-23 15:00:44 +0500

    task solution 9

--- style.js
+++ style.js
@@ -264,3 +264,36 @@ zombieShootout(97, 18, 60)
 // For this game of BINGO, you will receive a single array of 10 numbers from 1 to 26 as an input. Duplicate numbers within the array are possible.
 
 // Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc). Write a function where you will win the game if your numbers can spell "BINGO". They do not need to be in the right order in the input array. Otherwise you will lose. Your outputs should be "WIN" or "LOSE" respectively.
+
+// B - 2
+// I - 9
+// N - 14
+// G - 7
+// O - 15
+
+ //################ SOLUTION
+
+
+ function bingo(a) {
+	let arr = [2,9,14,7,15];
+	let counter = 0;
+		for (let i = 0; i < arr.length; i++) {
+			for (let k = 0; k < a.length; k++) {
+				if (arr[i] == a[k]) {
+					counter++
+					break;
+				}
+			}
+		}
+		if (counter == 5) {
+			return "WIN"
+		}
+		else{
+			return "LOSE"
+		}
+ }
+
+ bingo([1,2,3,4,5,6,7,8,9,10])
+ bingo([21,13,2,7,5,14,7,15,9,10])
+
+
