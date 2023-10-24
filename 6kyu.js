257ea1e3c8eb8d64a29cd334e2a09047a5f8559e
RijukNikola <kola1997@icloud.com>
2023-10-24 15:39:46 +0500

    task solution 1 6kyu

--- 6kyu.js
+++ 6kyu.js
@@ -1,3 +1,5 @@
+// 1
+
 // You are writing a chat room app for your company.
 
 // Users have provided feedback that seeing everyone's full names on the screen creates too much noise, and have asked for it to be reduced. However, we still want to be able to uniquely identify everyone. The team have come up with a few rules that will hopefully help in solving this problem!
@@ -21,3 +23,66 @@
 // For our purposes, when we receive the names casing is not important, meaning JOHn SmiTh is equal to john smith, after we've tidied up the casings, these should be interpreted as the same name.
 
 // Our company also has an unusual policy - we don't hire somebody if an employee already exists with the same full name, as our HR platform identifies staff by a combination of their first and last names. Due to this, we don't have to worry about multiple instances of the same full name.
+
+
+//################ SOLUTION
+
+
+function generateChatRoomNames(users) {
+	const getName = (name) => {
+		let space = name.indexOf(' ');
+		return (
+			name[0].toUpperCase()
+			+ name.slice(1, space).toLowerCase()
+			+ ' '
+			+ name[space + 1].toUpperCase()
+			+ name.slice(space + 2).toLowerCase()
+		)
+	};
+	const getAlias = (name) => {
+		let space = name.indexOf(' ');
+		return name.slice(0, space + 2);
+	}
+
+	const getFirstName = (name) => {
+		let space = name.indexOf(' ');
+		return name.slice(0, space);
+	}
+
+	const correctUsers = users.map(getName);
+	const cache = correctUsers.reduce((acc, user) => {
+		let alias = getAlias(user);
+		let firstName = getFirstName(user);
+		if (!acc.withNames[firstName]) {
+			acc.withAliases[alias] = 1;
+			acc.withNames[firstName] = 1;
+		} else {
+			acc.withNames[firstName]++;
+			if (!acc.withAliases[alias]) {
+				acc.withAliases[alias] = 1;
+			} else {
+				acc.withAliases[alias]++;
+			}
+		}
+		return acc;
+	}, { withAliases: {}, withNames: {} });
+
+	const res = [];
+	correctUsers.forEach(user => {
+		let alias = getAlias(user);
+		let firstName = getFirstName(user);
+		if (cache.withNames[firstName] === 1) {
+			res.push(firstName);
+		} else if (cache.withAliases[alias] === 1) {
+			res.push(alias);
+		} else {
+			res.push(user)
+		}
+
+	});
+
+	return res.sort();
+}
+
+
+generateChatRoomNames(["Joe Bloggs", "John Smith", "Jane Doe", "Jane Bloggs"]);
