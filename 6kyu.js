e56cc6f9f753ffabfa5c275f6d23153e93d4da90
RijukNikola <kola1997@icloud.com>
2023-10-30 16:50:31 +0500

    task 6 6 kyu

--- 6kyu.js
+++ 6kyu.js
@@ -264,3 +264,17 @@ function findOutlier(integers) {
 findOutlier([194164677,60511777,176440559,185868678,-78895983,-135863659])
 findOutlier([1, 2, 3])
 findOutlier([0, 1, 2])
+
+// 7
+
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+// "indivisibility" -> 1 # 'i' occurs six times
+// "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+// "aA11" -> 2 # 'a' and '1'
+// "ABBA" -> 2 # 'A' and 'B' each occur twice
